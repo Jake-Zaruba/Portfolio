9bd5590623155971e906bbc40abca517054fa717
Jake-Zaruba <zarubajake@gmail.com>
2022-05-10 18:02:28 -0500

    Added
    Added some basic Javascript, will be removing next commit

--- Portfolio Site/index-script.js
+++ Portfolio Site/index-script.js
@@ -1,1 +1,7 @@
 "strict mode";
+const css = document.querySelector(`.css`);
+const cssBtn = document.querySelector(`.css-btn`);
+
+cssBtn.addEventListener(`click`, function () {
+  css.classList.remove(`css`);
+});
