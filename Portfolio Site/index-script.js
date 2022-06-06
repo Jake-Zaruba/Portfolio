8ee4da75409acbb7b579ee25d10d432d29bc47ca
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2022-06-05 21:57:25 -0500

    Major Changes
    Added the employment, about, and contact sections.
    
    Also added animations throughout the whole site for section titles and the employment section.
    
    Increased visibility on the CTA "contact" button.
    
    Made the nav-bar sticky and modified to be readable throughout the site.

--- Portfolio Site/index-script.js
+++ Portfolio Site/index-script.js
@@ -1,8 +1,8 @@
 "strict mode";
 
-//////////////////////////////////////
-//PROJECT FOCUS////////////////////
-////////////////////////////////////
+// //////////////////////////////////////
+// //PROJECT FOCUS////////////////////
+// ////////////////////////////////////
 
 const project = document.querySelectorAll(`.project`);
 
@@ -23,7 +23,7 @@ project.forEach((project) => {
 
 //////////////////////////////////////
 //BACKGROUND FOCUS////////////////////
-////////////////////////////////////
+//////////////////////////////////////
 
 const card = document.querySelectorAll(`.cards`);
 
@@ -40,4 +40,157 @@ const cardObserver = new IntersectionObserver(
 
 card.forEach((card) => {
   cardObserver.observe(card);
+});
+
+///////////////////////////////////////
+//TIMELINE OBSERVER////////////////////
+///////////////////////////////////////
+
+const timelineObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add(`timeline-animation`, entry.isIntersecting);
+        return;
+      }
+      entry.target.classList.remove(`timeline-animation`);
+    });
+  },
+  {
+    threshold: 0.8,
+  }
+);
+
+timelineObserver.observe(document.querySelector(`.timeline-line`));
+
+///////////////////////////////////////
+//JOB OBSERVERS////////////////////////
+///////////////////////////////////////
+
+const job1Observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add(`job-1-animation`, entry.isIntersecting);
+        return;
+      }
+      entry.target.classList.remove(`job-1-animation`);
+    });
+  },
+  {
+    threshold: 0.8,
+  }
+);
+
+job1Observer.observe(document.getElementById(`job-1`));
+
+/////////
+
+const job2Observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add(`job-2-animation`, entry.isIntersecting);
+        return;
+      }
+      entry.target.classList.remove(`job-2-animation`);
+    });
+  },
+  {
+    threshold: 0.8,
+  }
+);
+
+job2Observer.observe(document.getElementById(`job-2`));
+
+/////////
+
+const job3Observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add(`job-3-animation`, entry.isIntersecting);
+        return;
+      }
+      entry.target.classList.remove(`job-3-animation`);
+    });
+  },
+  {
+    threshold: 0.8,
+  }
+);
+
+job3Observer.observe(document.getElementById(`job-3`));
+
+///////////////////////////////////////
+//WEBSITE SECTION UNDERLINE ///////////
+///////////////////////////////////////
+
+const home = document.getElementById(`home`);
+const projects = document.getElementById(`projects`);
+const technologies = document.getElementById(`technology`);
+const employment = document.getElementById(`employment`);
+const about = document.getElementById(`about`);
+const contact = document.getElementById(`contact`);
+/////////
+const projectsTitle = document.getElementById(`projects-title`);
+const technologiesTitle = document.getElementById(`technologies-title`);
+const employmentTitle = document.getElementById(`employment-title`);
+const aboutTitle = document.getElementById(`about-title`);
+const contactTitle = document.getElementById(`contact-title`);
+
+const section = document.querySelectorAll(`section`);
+const linkObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        let currentSection = entry.target.id;
+        if (currentSection === `home-section`) {
+          home.classList.add(`section-active`);
+        } else {
+          home.classList.remove(`section-active`);
+        }
+        if (currentSection === `projects-section`) {
+          projects.classList.add(`section-active`);
+          projectsTitle.classList.add(`title-animation`);
+        } else {
+          projects.classList.remove(`section-active`);
+          // projectsTitle.classList.remove(`title-animation`);
+        }
+        if (currentSection === `technologies-section`) {
+          technologies.classList.add(`section-active`);
+          technologiesTitle.classList.add(`title-animation`);
+        } else {
+          technologies.classList.remove(`section-active`);
+          // technologiesTitle.classList.remove(`title-animation`);
+        }
+        if (currentSection === `employment-section`) {
+          employment.classList.add(`section-active`);
+          employmentTitle.classList.add(`title-animation`);
+        } else {
+          employment.classList.remove(`section-active`);
+          // employmentTitle.classList.remove(`title-animation`);
+        }
+        if (currentSection === `about-section`) {
+          about.classList.add(`section-active`);
+          aboutTitle.classList.add(`title-animation`);
+        } else {
+          about.classList.remove(`section-active`);
+        }
+        if (currentSection === `contact-section`) {
+          contact.classList.add(`section-active`);
+          contactTitle.classList.add(`title-animation`);
+        } else {
+          contact.classList.remove(`section-active`);
+        }
+      }
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
+
+section.forEach((section) => {
+  linkObserver.observe(section);
 });
